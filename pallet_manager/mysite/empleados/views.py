a15c820e1515208f15ab8fd19b3966036127b03f
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-10-10 11:46:18 -0300

    creando un hola mundo de la vista empleados

--- pallet_manager/mysite/empleados/views.py
+++ pallet_manager/mysite/empleados/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def listar_empleados(request):
+    empleados = []
+    return render(request, 'empleados/lista_empleados.html', {'empleados': empleados})
