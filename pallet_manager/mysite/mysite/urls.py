bef2255b6aced28a9641b1e9e3aed9cef5d21a4f
agustin-ovejero <ovejeroagustin2@gmail.com>
2025-10-10 17:54:11 -0300

    se agrega el morph y funca

--- pallet_manager/mysite/mysite/urls.py
+++ pallet_manager/mysite/mysite/urls.py
@@ -19,6 +19,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from users.views import index
 
 urlpatterns = [
     path("movimientos/", include("movimientos.urls")),
@@ -26,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('empleados/', include('empleados.urls'))
+    path('empleados/', include('empleados.urls')),
+    path('', index, name='index'),
 ]
